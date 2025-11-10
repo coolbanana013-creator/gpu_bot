88d35f381205fc737624826361d95ebe078ed655
Alex4nd3r3 <164812882+Alex4nd3r3@users.noreply.github.com>
2025-11-11 00:33:59 +0100

    v 1.4.0

--- src/ga/evolver_compact.py
+++ src/ga/evolver_compact.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:583b620c7b3cf8c2b82b0fd129c75abdd645d16a74889e4298c94c2699989508
-size 45423
+oid sha256:289be6e03b98ddaa6c88c2c8370287e0eab5fd4b064907b5c2abb4ba86b4046b
+size 46138
